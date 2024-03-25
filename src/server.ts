49bdbc15e5d7e3e10d82aa9e1adc2c5320233bb2
Onur <ob-vest@hotmail.com>
2024-03-25 14:12:41 +0100

    added allow origin for dev env

--- src/server.ts
+++ src/server.ts
@@ -11,7 +11,9 @@ app.use(express.json());
 app.use((req, res, next) => {
   res.header(
     "Access-Control-Allow-Origin",
-    "https://roadmap-website-one.vercel.app"
+    process.env.NODE_ENV === "production"
+      ? "https://roadmap-website-one.vercel.app"
+      : "http://localhost:5173"
   );
   res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
   res.header(
